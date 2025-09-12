c3da4556ba7a2fed1b806a83e0bf726bcea563c3
ThinhNT <>
2025-09-12 11:13:29 +0700

    update abc

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TNTKit",
-            url: "https://github.com/AnhGio/TNTKit/releases/download/1.0.0/TNTKit.xcframework.zip",
+            url: "https://github.com/AnhGio/TNTKit/releases/download/1.0.1/TNTKit.xcframework.zip",
             checksum: "62024e20deae774cbbd853311a87fe9ebe4793b7b41d63a4ca8fd7ad44dd1db6"
         )
     ]
